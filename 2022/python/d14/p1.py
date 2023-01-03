1e18014ec4cebd48f1d89cbba917a896f654240d
Justin Mitchell <me@justin-mitchell.com>
2023-01-03 02:28:23 -0500

    blah

--- 2022/python/d14/p1.py
+++ 2022/python/d14/p1.py
@@ -1,60 +1,38 @@
 import re
-import collections
 import numpy as np
 
-test = open('input').read()
-max_y = 0
-grid = set()
 
-for l in test.split('\n'):
-    m = re.findall("\d+,\d+", l)
-    cur = m.pop(0)
-    cur = int(cur.split(',')[0]) + 1j * int(cur.split(',')[1])
-    while m:
-        dest = m.pop(0)
-        dest = int(dest.split(',')[0]) + 1j * int(dest.split(',')[1])
-        while dest != cur:
-            max_y = max(max_y, cur.imag)
-            grid.add(cur)
-            cur += np.sign((dest - cur).real) + 1j * np.sign((dest - cur).imag)
-        grid.add(cur)
-        max_y = max(max_y, cur.imag)
-        cur = dest
+def coord_to_complex(s):
+    return int(s.split(',')[0]) + 1j * int(s.split(',')[1])
 
-grid2 = grid.copy()
 
-kernels = 0
-sand = 500
-while sand.imag < 50000:
-    if not sand + 1j in grid:
+depth, beach = 0, set()
+for line in open('input').read().split('\n'):
+    coords = re.findall("\d+,\d+", line)
+    p0 = coord_to_complex(coords.pop(0))
+    while coords:
+        p1 = coord_to_complex(coords.pop(0))
+        while p0 != p1:
+            depth = max(depth, p0.imag)
+            beach.add(p0)
+            p0 += np.sign((p1 - p0).real) + 1j * np.sign((p1 - p0).imag)
+        beach.add(p0)
+        p0, depth = p1, max(depth, p1.imag)
+
+
+part1, part2, sand = 0, 0, 500
+while not 500 in beach:
+    if sand.imag == depth + 1:
+        beach.add(sand)
+        sand, part1, part2 = 500, part1 if part1 else part2, part2 + 1
+    elif not sand + 1j in beach:
         sand += 1j
-    elif sand - 1 + 1j not in grid:
+    elif sand - 1 + 1j not in beach:
         sand += (-1 + 1j)
-    elif sand + 1 + 1j not in grid:
+    elif sand + 1 + 1j not in beach:
         sand += 1 + 1j
     else:
-        grid.add(sand)
-        sand = 500
-        kernels += 1
+        beach.add(sand)
+        sand, part2 = 500, part2 + 1
 
-print(kernels)
-
-kernels = 0
-sand = 500
-while not 500 in grid2:
-    if sand.imag == max_y + 1:
-        grid2.add(sand)
-        sand = 500
-        kernels += 1
-    elif not sand + 1j in grid2:
-        sand += 1j
-    elif sand - 1 + 1j not in grid2:
-        sand += (-1 + 1j)
-    elif sand + 1 + 1j not in grid2:
-        sand += 1 + 1j
-    else:
-        grid2.add(sand)
-        sand = 500
-        kernels += 1
-
-print(kernels)
+print(part1, part2)
