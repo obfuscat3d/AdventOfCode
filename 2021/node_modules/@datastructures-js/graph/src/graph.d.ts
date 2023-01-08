80c04d68365390b93fb2a9ed85c937d01d6812ed
Justin Mitchell <me@justin-mitchell.com>
2023-01-08 09:01:04 -0500

    blah

--- 2021/node_modules/@datastructures-js/graph/src/graph.d.ts
+++
@@ -1,6 +0,0 @@
-import { DirectedGraph } from '../src/directedGraph';
-
-export class Graph<T extends number|string, U = undefined> extends DirectedGraph<T, U> {
-  addVertex(key: T, value: U): Graph<T, U>;
-  addEdge(srcKey: T, destKey: T, weight?: number): Graph<T, U>;
-}
