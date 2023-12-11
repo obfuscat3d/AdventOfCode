652403ba8d714d23e913cba846d850a6fa534a88
Justin Mitchell <me@justin-mitchell.com>
2023-12-11 13:54:46 -0500

    added quadratic method for solving d6

--- 2023/python/d6/p1.py
+++ 2023/python/d6/p1.py
@@ -1,4 +1,4 @@
-import re
+import re, math
 
 
 def parse(text):
@@ -7,12 +7,25 @@ def parse(text):
     return times, record_distances
 
 
-def go(times, record_distances):
+def brute_force(times, record_distances):
     winnings_combos = 1
     for t, d in zip(times, record_distances):
         winnings_combos *= sum(1 for c in range(1, t) if c * (t - c) > d)
     return winnings_combos
 
 
-print(go(*parse(open("input").read().split("\n"))))
-print(go(*parse(open("input").read().replace(" ", "").split("\n"))))
+def quadratic(times, record_distances):
+    winnings_combos = 1
+    for t, d in zip(times, record_distances):
+        high = (t + math.sqrt(t**2 - 4 * d)) / 2
+        low = (t - math.sqrt(t**2 - 4 * d)) / 2
+        high = math.floor(high) - (1 if int(high) == high else 0)
+        low = math.ceil(low) + (1 if int(low) == low else 0)
+        winnings_combos *= high - low + 1
+    return winnings_combos
+
+
+print(brute_force(*parse(open("input2").read().split("\n"))))
+print(quadratic(*parse(open("input2").read().split("\n"))))
+print(brute_force(*parse(open("input").read().replace(" ", "").split("\n"))))
+print(quadratic(*parse(open("input").read().replace(" ", "").split("\n"))))
